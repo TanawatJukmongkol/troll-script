0a19be46b9a91b07d4c843886c97d4b6718b382f
Tanawat Jukmongkol <tjukmong@z3t3c1.42bangkok.com>
2024-10-30 22:22:46 +0700

    done

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 
 # Copy startup scripts
+echo copy autostart scripts
+mkdir -p ~/.config/autostart
 cp assets/desktop/* ~/.config/autostart
-
+echo replace full path...
 sed -i 's|$HOME|'$HOME'|g' ~/.config/autostart/login-sound.desktop
 sed -i 's|$HOME|'$HOME'|g' ~/.config/autostart/activate-linux.desktop
 
 # Copy soundfonts
+echo copy to soundfont to ~/.local/share/sounds
+mkdir -p ~/.local/share/sounds
 cp -r assets/sounds/WinXP ~/.local/share/sounds
+echo set soundfont to WinXP
+gsettings set org.gnome.desktop.sound event-sounds true
+gsettings set org.gnome.desktop.sound theme-name 'WinXP'
 
 # Copy binary to ~/.local
+echo copy binary to ~/.local
+mkdir -p ~/.local/bin
 cp -f bin/* ~/.local/bin
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,20 @@
 
-# Copy startup scripts
-rm ~/.config/autostart/login-sound.desktop
-rm ~/.config/autostart/activate-linux.desktop
+# Remove startup scripts
+echo remove startup scripts
+rm -f ~/.config/autostart/login-sound.desktop
+rm -f ~/.config/autostart/activate-linux.desktop
 
-# Copy soundfonts
+# Remove soundfont
+echo remove soundfont
 rm -rf ~/.local/share/sounds/WinXP
+echo set soundfont to default
+gsettings set org.gnome.desktop.sound theme-name 'default'
 
-# Copy binary to ~/.local
+# Remove binary from ~/.local
+echo remove binary from ~/.local
 rm -f ~/.local/bin/activate-linux
+
+# Kill troll binary
+echo Kill troll binary
+pkill activate-linux
 
